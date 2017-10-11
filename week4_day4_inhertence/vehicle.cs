3ecac27f4ffb409df3defcf9a60e7b0a3e6b409e
yusuf-farah <yfarah1577@gmail.com>
2017-10-11 12:41:59 -0400

    added money codes

---
+++ week4_day4_inhertence/vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week4_day4_inhertence
+{
+    class vehicle
+    {
+    }
+}
